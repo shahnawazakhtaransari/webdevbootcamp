ec78346b4dc15ccb8209fc886b1cc58bc10255fe
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-27 18:55:45 +0530

    first commit

---
+++ 10.0javascript/trial.js
@@ -0,0 +1,30 @@
+function getMilk() {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+  }
+
+
+
+//...........LOVE CALCULATOR...........
+//   var one=prompt("Enter your name!");
+// var two=prompt("Enter your lovers name!");
+// var n=Math.random();
+// n=n*100;
+// n=Math.round(n)+10;
+// alert(n+"% is love score for "+one+" and "+two);
